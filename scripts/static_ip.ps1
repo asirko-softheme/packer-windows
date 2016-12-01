945cbaddbd5ad4c52447269306219ee5fc1b8977
asirko-softheme <alexander.sirko@softheme.com>
2016-12-01 04:57:14 -0800

    set static ip

---
+++ scripts/static_ip.ps1
@@ -0,0 +1,27 @@
+$IP = "192.168.64.108"
+$MaskBits = 19 # This means subnet mask = 255.255.224.0
+$Gateway = "192.168.64.3"
+$Dns = "10.250.240.4"
+$IPType = "IPv4"
+
+# Retrieve the network adapter that you want to configure
+$adapter = Get-NetAdapter | ? {$_.Status -eq "up"}
+
+# Remove any existing IP, gateway from our ipv4 adapter
+If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
+    $adapter | Remove-NetIPAddress -AddressFamily $IPType -Confirm:$false
+}
+
+If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+    $adapter | Remove-NetRoute -AddressFamily $IPType -Confirm:$false
+}
+
+ # Configure the IP address and default gateway
+$adapter | New-NetIPAddress `
+    -AddressFamily $IPType `
+    -IPAddress $IP `
+    -PrefixLength $MaskBits `
+    -DefaultGateway $Gateway
+
+# Configure the DNS client server IP addresses
+$adapter | Set-DnsClientServerAddress -ServerAddresses $DNS
