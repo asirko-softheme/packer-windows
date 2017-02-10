1dff9bc5d709d86c9aa8bc0f020ad2d4e9350bf4
asirko-softheme <alexander.sirko@softheme.com>
2017-02-10 09:15:48 -0800

    latest fixed

--- hyperv/scripts/install-packer.ps1
+++
@@ -1,4 +0,0 @@
-cd $env:TEMP
-wget -outfile Packer.zip -uri https://dl.bintray.com/taliesins/Packer/Packer.1.0.0.145-HyperV.nupkg -UseBasicParsing
-Expand-Archive packer.zip
-copy packer\packer.exe $env:ChocolateyInstall\bin\packer.exe
--- scripts/apps.bat
+++
@@ -1,4 +0,0 @@
-choco install notepadplusplus.install -y
-choco install 7zip.install -y
-choco install dotnet4.5 -y
-choco install powershell -y
